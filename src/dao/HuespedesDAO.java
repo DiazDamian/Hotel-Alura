317a8978b3cd14d9f3a6cba657ad2bbd7704bac1
DiazDamian <130494732+DiazDamian@users.noreply.github.com>
2023-09-18 16:14:52 -0300

    huespedes dao actualizado

--- src/dao/HuespedesDAO.java
+++ src/dao/HuespedesDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,11 +20,11 @@ public class HuespedesDAO {
 	
 	public List<Huespedes> buscarHuespedes(){
 		List<Huespedes> resultado = new ArrayList<>();
-		String query="SELECT id, nombre, apellido, fechaNacimiento, nacionalidad, telefono, idReserva FROM huespedes";
+		String query="SELECT id, nombre, apellido, fechaDeNacimiento, nacionalidad, telefono, idReserva FROM huespedes";
 		try {
 			final PreparedStatement state = con.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
 				try (state){
-					ResultSet res = state.getResultSet();
+					ResultSet res = state.executeQuery();
 					while(res.next()) {
 						Huespedes huesped = new Huespedes(res.getInt(1),res.getString(2),res.getString(3),res.getDate(4),res.getString(5),res.getString(6),res.getInt(7));
 						resultado.add(huesped);
@@ -33,5 +34,59 @@ public class HuespedesDAO {
 			throw new RuntimeException(e);
 		}
 		return resultado;
+	}
+	
+	public void eliminar(Integer id) {
+		String query="DELETE FROM huespedes WHERE id = ?";
+		try {
+			final PreparedStatement state = con.prepareStatement(query);
+			try(state){
+				state.setInt(1, id);
+				state.execute();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void guardar(Huespedes huesped) {
+		String query="INSERT INTO huespedes(nombre,apellido,fechaDeNacimiento,nacionalidad,telefono,idReserva) VALUES (?,?,?,?,?,?)";
+		try {
+			final PreparedStatement state = con.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+			state.setString(1, huesped.getNombre());
+			state.setString(2, huesped.getApellido());
+			state.setDate(3, huesped.getFechaNacimiento());
+			state.setString(4, huesped.getNacionalidad());
+			state.setString(5, huesped.getTelefono());
+			state.setInt(6, huesped.getIdReserva());
+			state.executeUpdate();
+			try (final ResultSet res=state.getGeneratedKeys()){
+				while(res.next()) {
+					huesped.setId(res.getInt(1));
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void actualizarHuesped(String nombre,String apellido,Date fechaNacimiento,String nacionalidad,String telefono,Integer idReserva,Integer id) {
+		String query="UPDATE huespedes SET nombre = ?, apellido = ?, fechaDeNacimiento = ?, nacionalidad = ?, telefono = ?, idReserva = ? WHERE id = ?";
+		try {
+			final PreparedStatement state = con.prepareStatement(query);
+			state.setString(1, nombre);
+			state.setString(2, apellido);
+			state.setDate(3, fechaNacimiento);
+			state.setString(4, nacionalidad);
+			state.setString(5, telefono);
+			state.setInt(6, idReserva);
+			state.setInt(7, id);
+			state.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		}
 	}
 }
--- src/factory/ConnectionTest.java
+++ src/factory/ConnectionTest.java
@@ -1,9 +1,16 @@
 package factory;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import dao.HuespedesDAO;
 import dao.ReservasDAO;
@@ -11,7 +18,7 @@ import modelos.Huespedes;
 import modelos.Reservas;
 
 public class ConnectionTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 	
 		ConnectionFactory conFactory = new ConnectionFactory();
 		Connection con = conFactory.conectar();
@@ -19,14 +26,30 @@ public class ConnectionTest {
 		HuespedesDAO hdao = new HuespedesDAO(con);
 		try {
 			System.out.println("probando conexion");
-			List<Reservas> reservas = new ArrayList<>();
-			reservas = rdao.buscarReservas();
-			List<Huespedes> huespedes =new ArrayList<>();
-			huespedes = hdao.buscarHuespedes();
+			
+			//List<Reservas> reservas = new ArrayList<>();
+			//reservas = rdao.buscarReservas();
+			
+			//List<Huespedes> huespedes =new ArrayList<>();
+			//huespedes = hdao.buscarHuespedes();
+			
+			/*
+			Date fechaE=new Date(123,05,13) ;
+			Date fechaS = new Date(124,06,02);
+			Reservas res= new Reservas(fechaE,fechaS,"5000","Dinero en efectivo");
+			rdao.actualizarReserva(fechaE, fechaS, "2000", "Dinero en efectivo", 5);
+			//rdao.guardarReserva(res);
+			*/
+			Huespedes huesped = new Huespedes("pablo","guerrez",new Date(123,2,2),"argentina","123456789",2);
+			//hdao.guardar(huesped);
+			hdao.actualizarHuesped(huesped.getNombre(), huesped.getApellido(), huesped.getFechaNacimiento(), huesped.getNacionalidad(), huesped.getTelefono(), huesped.getIdReserva(), 6);
+			//rdao.eliminarReserva(10);
+			
+			
 			con.close();
 			
-			System.out.println(reservas.toString());
-			System.out.println(huespedes.toString());
+			//System.out.println(reservas.toString());
+			//System.out.println(huespedes.get(0).getApellido());
 			System.out.println("conexion cerrada");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
