50b629dc1b5fcde767ff251a64acd5491afd98b6
DiazDamian <130494732+DiazDamian@users.noreply.github.com>
2023-09-18 02:21:59 -0300

    agregando huespedes dao

--- src/dao/HuespedesDAO.java
+++ src/dao/HuespedesDAO.java
@@ -1,5 +1,37 @@
 package dao;
 
-public class HuespedesDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import modelos.Huespedes;
+
+public class HuespedesDAO {
+	Connection con;
+	
+	public HuespedesDAO(Connection con) {
+		this.con=con;
+	}
+	
+	public List<Huespedes> buscarHuespedes(){
+		List<Huespedes> resultado = new ArrayList<>();
+		String query="SELECT id, nombre, apellido, fechaNacimiento, nacionalidad, telefono, idReserva FROM huespedes";
+		try {
+			final PreparedStatement state = con.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+				try (state){
+					ResultSet res = state.getResultSet();
+					while(res.next()) {
+						Huespedes huesped = new Huespedes(res.getInt(1),res.getString(2),res.getString(3),res.getDate(4),res.getString(5),res.getString(6),res.getInt(7));
+						resultado.add(huesped);
+					}
+				}
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return resultado;
+	}
 }
--- src/factory/ConnectionTest.java
+++ src/factory/ConnectionTest.java
@@ -5,7 +5,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import dao.HuespedesDAO;
 import dao.ReservasDAO;
+import modelos.Huespedes;
 import modelos.Reservas;
 
 public class ConnectionTest {
@@ -14,15 +16,17 @@ public class ConnectionTest {
 		ConnectionFactory conFactory = new ConnectionFactory();
 		Connection con = conFactory.conectar();
 		ReservasDAO rdao = new ReservasDAO(con);
-		
+		HuespedesDAO hdao = new HuespedesDAO(con);
 		try {
 			System.out.println("probando conexion");
 			List<Reservas> reservas = new ArrayList<>();
 			reservas = rdao.buscarReservas();
+			List<Huespedes> huespedes =new ArrayList<>();
+			huespedes = hdao.buscarHuespedes();
 			con.close();
 			
 			System.out.println(reservas.toString());
-			
+			System.out.println(huespedes.toString());
 			System.out.println("conexion cerrada");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
